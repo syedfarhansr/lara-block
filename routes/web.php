eb3c2eaa1d1b26c9978bb06cd09264ff2638fdac
syed farhan sr <syedfarhan17861@gmail.com>
2024-01-31 23:31:09 +0530

    route update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Facades\File as FacadesFile;
 use Illuminate\Support\Facades\Route;
+use League\CommonMark\Extension\FrontMatter\Data\LibYamlFrontMatterParser;
+use Spatie\Backtrace\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Symfony\Component\Yaml\Yaml;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +20,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+
+    return view('posts', [
+        'posts' => Post::all()
+
+    ]);
 });
+
+
+
+Route::get('posts/{post}', function ($slug) {
+
+    $post = Post::find($slug);
+
+    return view('post', [
+        'post' => $post
+    ]);
+})->where('post', '[A-z_\-]+');
