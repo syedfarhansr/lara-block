e66a60cd222987cea38789ddca898302fa659ae2
syed farhan sr <syedfarhan17861@gmail.com>
2024-02-02 02:43:25 +0530

    posts.blade.php

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+
+     @foreach ($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/<?= $post->slug; ?>">
+                <?= $post->title; ?>
+            </a>
+            </h1>
+            <div>
+                {{ $post->body }}
+            </div>
+            </article>
+            @endforeach
+</x-layout>
