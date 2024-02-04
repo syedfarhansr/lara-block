94b7d3a3ff515345788bc699af7bf29159ec684f
syed farhan sr <syedfarhan17861@gmail.com>
2024-02-04 14:00:20 +0530

    web.php.routes

--- routes/web.php
+++ routes/web.php
@@ -31,10 +31,7 @@ Route::get('/', function () {
 
 
 Route::get('posts/{post}', function ($slug) {
-
-    $post = Post::find($slug);
-
     return view('post', [
-        'post' => $post
+        'post' => Post::findorfail($slug)
     ]);
-})->where('post', '[A-z_\-]+');
+});
